a67e367fefecc541fcbb03c065dfdab7ec368ed5
mike <viriyananda.michael@gmail.com>
2016-12-19 11:22:30 +0700

    Adding new module product_certificate

---
+++ product_certificate/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Certificate",
+    "version": "8.0.1.0.0",
+    "summary": "Base Product Certficate Module.",
+    "category": "Product",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [],
+    "data": [],
+}
---
+++ product_certificate/models/product_certificate.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+from openerp.tools.translate import _
+from openerp.exceptions import Warning as UserError
+
+
+class ProductCertificate(models.AbstractModel):
+    _name = "product.certificate"
+    _description = "Product Certificate"
+    _order = "date_start desc"
+
+    name = fields.Char(
+        string="# Certificate",
+        required=True,
+    )
+    issuer_id = fields.Many2one(
+        string="Issuer",
+        comodel_name="res.partner",
+        )
+    date_start = fields.Date(
+        string="Start Date",
+        required=True
+    )
+    date_end = fields.Date(
+        string="End Date"
+    )
+
+    @api.constrains(
+        "date_start", "date_end")
+    def _check_date(self):
+        strWarning = _(
+            "'Date End' must be greater than 'Date Start'")
+        if self.date_start and self.date_end:
+            if self.date_start > self.date_end:
+                raise UserError(strWarning)
