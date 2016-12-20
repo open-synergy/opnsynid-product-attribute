1c4b25e67d4e2dc6e5f19621b1794f2b5966555d
mike <viriyananda.michael@gmail.com>
2016-12-20 11:15:36 +0700

    [UPD]Based on review

--- product_halal_certificate/__openerp__.py
+++ product_halal_certificate/__openerp__.py
@@ -2,18 +2,15 @@
 # Copyright 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Product Halal",
+    "name": "Product's Halal Certificate Management",
     "version": "8.0.1.0.0",
-    "summary": "Product's halal certificate information",
+    "summary": "Manage product's halal certificate information",
     "category": "Product",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
-    "depends": [
-        "product",
-        "product_certificate"
-    ],
+    "depends": ["product_certificate"],
     "data": [
         "security/ir.model.access.csv",
         "views/product_template_view.xml"
