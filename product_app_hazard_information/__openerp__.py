5df0d4cdce82a8dc621324058055b18b7d677a0a
Andhitia Rama <andhitia.r@gmail.com>
2017-04-21 16:32:15 +0700

    [ADD][8.0] product_app_hazard_information

---
+++ product_app_hazard_information/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Management Application - Hazard Information Extension",
+    "version": "8.0.1.0.0",
+    "category": "Product",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "product_app",
+        "product_hazard_information",
+        ],
+    "data": [
+        "menu.xml",
+        ],
+}
