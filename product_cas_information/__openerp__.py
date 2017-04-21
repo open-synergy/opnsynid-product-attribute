bd430c77e337f228cc52504dbf7a4f8e8fb471e9
Andhitia Rama <andhitia.r@gmail.com>
2017-04-21 17:42:11 +0700

    [ADD][8.0] product_cas_information

---
+++ product_cas_information/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Product CAS Information",
+    "version": "8.0.1.0.0",
+    "category": "Product",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/product_cas_registry_views.xml",
+        "views/product_template_views.xml",
+        "views/product_product_views.xml"
+    ],
+}
