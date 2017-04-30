4ad54b5324a6158b547900575d970f6a968003a2
Andhitia Rama <andhitia.r@gmail.com>
2017-04-30 19:13:53 +0700

    [ADD][8.0] product_app_stock

---
+++ product_app_stock/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Product's Management Application - stock Extension",
+    "version": "8.0.1.0.0",
+    "category": "Product Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "application": True,
+    "depends": [
+        "product_app",
+        "stock",
+    ],
+    "data": [
+        "menu.xml",
+    ],
+}
