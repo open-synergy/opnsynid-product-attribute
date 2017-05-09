c41d9f3ffe43c2cebb8aaf18d4e409acb43bfb23
Andhitia Rama <andhitia.r@gmail.com>
2017-05-09 23:35:50 +0700

    sale_ok & purchase_ok is false when create new product

--- product_app/__openerp__.py
+++ product_app/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Product's Management Application",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "summary": "App to centralized roduct management",
     "category": "Product Management",
     "website": "https://opensynergy-indonesia.com",
