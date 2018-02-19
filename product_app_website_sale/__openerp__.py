c66070225b216e0445398407a676187799c3b4c3
mike <viriyananda.michael@gmail.com>
2018-02-19 12:49:50 +0700

    [8.0][product_app_website_sale] Adding New Module
    Revition

---
+++ product_app_website_sale/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Product's Management Application - website_sale Extension",
+    "version": "8.0.1.0.0",
+    "category": "Product Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "application": False,
+    "depends": [
+        "product_app",
+        "website_sale",
+    ],
+    "data": [
+        "menu.xml",
+    ],
+}
