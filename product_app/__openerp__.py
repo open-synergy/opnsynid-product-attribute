feb5ce06fa8271a8eee75889f2fefe067b564a82
Andhitia Rama <andhitia.r@gmail.com>
2018-04-12 22:28:42 +0700

    [8.0.1.3.0] product_app
    * Add feature to create menu per pricelist type

--- product_app/__openerp__.py
+++ product_app/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Product's Management Application",
-    "version": "8.0.1.2.0",
+    "version": "8.0.1.3.0",
     "summary": "App to centralized roduct management",
     "category": "Product Management",
     "website": "https://opensynergy-indonesia.com",
