5144522c93280bfb7f25bb561e2fa31cda2b2562
mikevhe18 <viriyananda.michael@gmail.com>
2018-08-10 08:52:39 +0700

    [8.0.1.0.0][product_app_sale_packaging_price]

---
+++ product_app_sale_packaging_price/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Product's Management Application - "
+            "Sale Packaging Price Extension",
+    "version": "8.0.1.0.0",
+    "category": "Product Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "application": True,
+    "depends": [
+        "product_app",
+        "sale_packaging_price",
+    ],
+    "data": [
+        "menu.xml",
+    ],
+}
