9dffb3be231abfc4abde1eb9bf1b4c03f56d6163
mikevhe18 <viriyananda.michael@gmail.com>
2019-04-23 09:12:10 +0700

    [8.0.1.0.0][product_app_document]

---
+++ product_app_document/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Product's Management Application - product_document Extension",
+    "version": "8.0.1.0.0",
+    "category": "Product Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "application": True,
+    "depends": [
+        "product_app",
+        "product_document",
+    ],
+    "data": [
+        "menu.xml",
+    ],
+}
