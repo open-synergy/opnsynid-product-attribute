2286cd7e47e55876b0052ee4ce1ee37bfbbb662e
Andhitia Rama <andhitia.r@gmail.com>
2019-09-05 19:49:01 +0700

    [8.0.1.0.0] product_prepaid_account

---
+++ product_prepaid_account/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Prepaid Income & Expense Account",
+    "version": "8.0.1.0.0",
+    "category": "Product",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "product",
+        "account",
+    ],
+    "data": [
+        "views/product_category_views.xml",
+        "views/product_template_views.xml",
+    ],
+}
---
+++ product_prepaid_account/models/product_category.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class ProductCategory(models.Model):
+    _inherit = "product.category"
+    _name = "product.category"
+
+    prepaid_income_account_id = fields.Many2one(
+        string="Prepaid Income Account",
+        comodel_name="account.account",
+        company_dependent=True,
+        domain=[
+            ("type", "=", "other"),
+        ],
+    )
+    prepaid_expense_account_id = fields.Many2one(
+        string="Prepaid Expense Account",
+        comodel_name="account.account",
+        company_dependent=True,
+        domain=[
+            ("type", "=", "other"),
+        ],
+    )
